a7bf5c7f552837702ce0e5f004f2707dc10a2859
Hugo ARNAL <hugo.arnal@epitech.eu>
2025-04-12 14:15:27 +0200

    feat: moved cs location in filesystem
    cs used to be at /usr/bin/cs but now is located at /usr/local/bin/cs

--- install.sh
+++ install.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/bash
 
+if [ -e "/usr/bin/cs" ]; then
+    line=$(head -n 1 "/usr/bin/cs")
+    if [ "$line" == "#!/usr/bin/env python3" ]; then
+        echo "You seem to have an outdated cs version located at /usr/bin/cs."
+        echo "It needs to be removed to install the new cs version."
+        echo "If the file located at /usr/bin/cs is not the Counter Style script, do not proceed."
+        read -p "Would you like to proceed? (y/n) " proceed
+        echo ""
+        if [ $proceed == "y" ]; then
+            echo "Proceeding with removing the /usr/bin/cs script."
+            sudo rm /usr/bin/cs
+        else
+            echo "Not proceeding with removing the /usr/bin/cs file."
+        fi
+    fi
+    echo ""
+fi
+
 echo "Downloading latest cs version:"
-sudo curl -o /usr/bin/cs https://raw.githubusercontent.com/hugoarnal/cs/main/cs
-sudo chmod +x /usr/bin/cs
+sudo curl -o /usr/local/bin/cs https://raw.githubusercontent.com/hugoarnal/cs/main/cs
+sudo chmod +x /usr/local/bin/cs
+echo ""
+
+echo "Make sure you have /usr/local/bin in your PATH variable for cs to run."
