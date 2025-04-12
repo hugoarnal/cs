6283912c0725067dc2e1156655a8646060472175
Hugo ARNAL <hugo.arnal@epitech.eu>
2025-04-12 14:37:50 +0200

    fix: remove prompting from install.sh
    Instead, forcing the user to manually remove the previous script

--- install.sh
+++ install.sh
@@ -5,17 +5,8 @@ if [ -e "/usr/bin/cs" ]; then
     if [ "$line" == "#!/usr/bin/env python3" ]; then
         echo "You seem to have an outdated cs version located at /usr/bin/cs."
         echo "It needs to be removed to install the new cs version."
-        echo "If the file located at /usr/bin/cs is not the Counter Style script, do not proceed."
-        read -p "Would you like to proceed? (y/n) " proceed
-        echo ""
-        if [ "$proceed" == "y" ]; then
-            echo "Proceeding with removing the /usr/bin/cs script."
-            sudo rm /usr/bin/cs
-        else
-            echo "Not proceeding with removing the /usr/bin/cs file."
-        fi
+        exit 1
     fi
-    echo ""
 fi
 
 echo "Downloading latest cs version:"
